95cac99126feb70f8572d43c3b74d04a5d650197
Marcio Leite <contato@marcioleitedev.com.br>
2025-07-25 16:50:06 -0300

    navbar

--- src/components/templates/Navbar.js
+++ src/components/templates/Navbar.js
@@ -1,48 +1,56 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { FaRegSmile, FaFileCode, FaGlasses, FaPhone } from 'react-icons/fa';
 
-import { FaRegSmile } from "react-icons/fa";
-import { FaFileCode } from "react-icons/fa";
-import { FaGlasses } from "react-icons/fa";
-import { FaPhone } from "react-icons/fa";
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <Link to="/" className="navbar-brand">
+        <img src="/image/logotipo.png" width="100" alt="Logotipo" />
+      </Link>
 
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Alternar navegação"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-
-
-
-function Navbar(){
-    return(
-
-        <div>
-        <nav className="navbar navbar-expand-lg navbar-dark ">
-  <a className="navbar-brand" ><Link to="/">
-    <img src="/image/logotipo.png" width="100" />
-     </Link></a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item">
-       <Link to="/"> <span className='linkagem'><FaRegSmile/> Sobre</span> </Link>
-      </li>
-      <li className="nav-item">
-         <Link to="/certificados"> <span className='linkagem'><FaFileCode/> Certificações</span></Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/portfolio">  <span className='linkagem'><FaGlasses/> Portfólio</span> </Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/servicos">  <span className='linkagem'><FaFileCode/> Serviços</span> </Link>
-      </li>
-      <li className="nav-item">
-       <Link to="/contato"><span className='linkagem'><FaPhone/> Contato</span> </Link>
-      </li>
-     
-    </ul>
-  </div>
-</nav>
-        </div>
-    )
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link linkagem">
+              <FaRegSmile /> Sobre
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/certificados" className="nav-link linkagem">
+              <FaFileCode /> Certificações
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/portfolio" className="nav-link linkagem">
+              <FaGlasses /> Portfólio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/servicos" className="nav-link linkagem">
+              <FaFileCode /> Serviços
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contato" className="nav-link linkagem">
+              <FaPhone /> Contato
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
