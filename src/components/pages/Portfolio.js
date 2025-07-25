0ee1c24028653c86a7e14bdea80e7ca723cf13c9
Marcio Leite <contato@marcioleitedev.com.br>
2025-07-25 16:32:01 -0300

    deploy

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -13,25 +13,41 @@ function Portfolio(){
 
         <section className="certificados">
         <h3>Portfólio</h3>
-        <p>Alguns dos projetos ainda em produção.</p>
+        <p>Um pouco sobre minha trajetória, contando projetos relevantes.</p>
 
         <div className="grade">
+
+              <div className="box" data-aos="fade-up">
+            <h2>Capital Consig</h2>
+            <p><b>Atuação:</b> 02/2023  até 06/2025 </p>
+            <p><b>Modalidade:</b> hibrido </p>
+            <p>Programador Full Stack </p>
+            <p>Atuando com demandas internas do banco atuando com as principais tecnologias </p>
+
+            <p><span className="laranja">TECNOLOGIA UTILIZADA</span> </p>
+            <p>Php , Laravel , Node.js , Python , Vue.js ,  Mysql, MariaDB , Apache , Nginx , Docker , Devops </p>
+            <a href="#" target="blank"><button>Projetos Internos </button></a>
+        </div>
+
 
         <div className="box" data-aos="fade-up">
             <h2>Sense Translate</h2>
             
-            <p>Sistema PHP MVC OOP </p>
+            <p>Sistema PHP MVC OOP (Laravel) </p>
+            <p><b>Atuação:</b> 02/2021   </p>
+            <p><b>Modalidade:</b> remoto </p>
             <p>Area Administrativa com níveis de usuário</p>
             <p>Árvore geneológica em Javascript</p>
             <p>Criação de Robo para trabalhos rotineiros (web scrapping)</p>
             <p><span className="laranja">TECNOLOGIA UTILIZADA</span> </p>
-            <p>Php , Javascript , Jquery , Mysql, MariaDB , Python , Devops </p>
+            <p>Php , Laravel , Javascript , Jquery , Mysql, MariaDB , Python , Devops </p>
             <a href="https://www.sensetranslate.com" target="blank"><button>Visitar Projeto</button></a>
         </div>
 
         <div className="box" data-aos="fade-up">
             <h2>CRM - Rastreadores Protege</h2>
-            
+            <p><b>Atuação:</b> 04/2004  até 12/2012 </p>
+            <p><b>Modalidade:</b> presencial </p>
             <p>Sistema PHP MVC OOP </p>
             <p>Area Administrativa com níveis de usuário</p>
             <p>CRM ( cadastro de clientes, fornecedores, controle de estoque, geração de contratos )</p>
@@ -42,6 +58,8 @@ function Portfolio(){
 
         <div className="box" data-aos="fade-up">
             <h2>CRM - Rastreadores INSAT</h2>
+             <p><b>Atuação:</b> 04/2004  até 12/2012 </p>
+            <p><b>Modalidade:</b> hibrido </p>
             <p>Site Institucional PHP framework Laravel</p>
             <p>Sistema PHP MVC OOP </p>
             <p>Area Administrativa com níveis de usuário</p>
@@ -51,25 +69,16 @@ function Portfolio(){
             <a href="https://www.rastreadoresinsat.com" target="blank"><button>Visitar Projeto</button></a>
         </div>
 
-                <div className="box" data-aos="fade-up">
-            <h2>Capital Consig</h2>
-            
-            <p>Programador Full Stack </p>
-            <p>Atuando com demandas internas do banco atuando com as principais tecnologias </p>
-
-            <p><span className="laranja">TECNOLOGIA UTILIZADA</span> </p>
-            <p>Php , Laravel , Node.js , Python , Vue.js ,  Mysql, MariaDB , Apache , Nginx , Docker , Devops </p>
-            <a href="#" target="blank"><button>Projetos Internos </button></a>
-        </div>
-
+              
                 <div className="box" data-aos="fade-up">
             <h2>Dflix Control</h2>
-            
+             <p><b>Atuação:</b> 07/2025   </p>
+            <p><b>Modalidade:</b> Projeto Próprio </p>
             <p>Criação de Saas (sistema de assinatura) para gestão de auto-center</p>
             <p>ERP para cadastro de clientes, controle de estoque , geração orçamento, geração ordem serviço ,fluxo de caixa, emissão de nota fiscal, sistema completo para gestão de auto centers.</p>
 
             <p><span className="laranja">TECNOLOGIA UTILIZADA</span> </p>
-            <p>Php , Laravel , Mysql, Vue.js ,   Nginx , Docker , Devops </p>
+            <p>Php , Laravel , Mysql, Vue.js ,   Nginx , Docker , Ubuntu ,  Devops </p>
             <a href="https://dflixcontrol.com.br" target="blank"><button>Projetos Internos </button></a>
         </div>
 
